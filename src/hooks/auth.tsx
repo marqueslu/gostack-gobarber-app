cea9946c6072e978ff5a5cdbe3b589498fa59da9
Lucas Marques <lucas.msilva09@gmail.com>
2020-07-18 16:57:41 -0300

    Adding authenticated routes

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -20,6 +20,7 @@ interface SignInCredentials {
 
 interface AuthContextData {
   user: object;
+  loading: boolean;
   signIn(credential: SignInCredentials): Promise<void>;
   signOut(): void;
 }
@@ -38,6 +39,7 @@ export function useAuth(): AuthContextData {
 
 export const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>({} as AuthState);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadStorageData(): Promise<void> {
@@ -49,6 +51,8 @@ export const AuthProvider: React.FC = ({ children }) => {
       if (token[1] && user[1]) {
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
+
+      setLoading(false);
     }
 
     loadStorageData();
@@ -76,7 +80,7 @@ export const AuthProvider: React.FC = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut, loading }}>
       {children}
     </AuthContext.Provider>
   );
