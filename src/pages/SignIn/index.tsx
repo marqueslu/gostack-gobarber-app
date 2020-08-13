6886e39ddb2870013abe24899ba7c82950abad25
Lucas Marques <lucas.msilva09@gmail.com>
2020-08-13 20:54:55 -0300

    Creating a basic setup of tests

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -95,7 +95,7 @@ const SignIn: React.FC = () => {
           contentContainerStyle={{ flex: 1 }}
         >
           <Container>
-            <Image source={logoImg} />
+            <Image testID="login-image" source={logoImg} />
             <View>
               <Title>Faça seu logon</Title>
             </View>
